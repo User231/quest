f16e18006177acd6760097a7faf926d6a17c8e6f
user231 <user23131@gmail.com>
2017-09-27 20:40:23 +0300

    both http and https

--- server1/src/public/chat.js
+++ server1/src/public/chat.js
@@ -1,6 +1,7 @@
 $(function () {
   
-  ws = new WebSocket(`ws://${location.host}`);
+  ws = undefined;
+  flag = 0;
 
   $('form').submit(function(){
     var text = $('#m').val();
@@ -49,8 +50,26 @@ $(function () {
     }
   };
 
-  ws.onerror = () => showTextMessage('WebSocket error');
-  ws.onopen = () => showTextMessage('WebSocket connection established');
-  ws.onclose = () => showTextMessage('WebSocket connection closed');
-  ws.onmessage = (event) => onMessage(event.data);
+  var establishConnection = () => {
+    var setHandlers = () => {
+      ws.onerror = () => {
+        showTextMessage('WebSocket error');
+        setTimeout(() => { establishConnection(); }, 10000);
+      };
+      ws.onopen = () => showTextMessage('WebSocket connection established');
+      ws.onclose = () => showTextMessage('WebSocket connection closed');
+      ws.onmessage = (event) => onMessage(event.data);
+    }
+    if (flag == 0) {
+      ws = new WebSocket(`wss://${location.host}`);
+      flag = 1;
+      setHandlers();
+    }
+    else if (flag == 1) {
+      ws = new WebSocket(`ws://${location.host}`);
+      flag = 0;
+      setHandlers();
+    }
+  }
+  establishConnection();
 });
